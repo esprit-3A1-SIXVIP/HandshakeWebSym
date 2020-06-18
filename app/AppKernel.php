0e69d8e85b48dd64cf2341764ece5d1e271f66c5
AlphaMalek <malek.taktak@esprit.tn>
2020-06-18 14:07:22 +0100

    Dernière Version avant le Travail de Samyra

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new RefugeBundle\RefugeBundle(),
+            new EvenementBundle\EvenementBundle(),
             new UserBundle\UserBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new FOS\CKEditorBundle\FOSCKEditorBundle(),
